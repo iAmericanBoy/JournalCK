be45e14a2a6983d6ae4e0da389747e337af893d1
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-02-25 15:13:40 -0700

    Adds EntryListTableViewController

---
+++ JournalCK/Controller/EntryListTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  EntryListTableViewController.swift
+//  JournalCK
+//
+//  Created by Dominic Lanzillotta on 2/25/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+import UIKit
+
+class EntryListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        EntryController.shared.fetchEntries { (success) in
+            if success {
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return EntryController.shared.entries.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "entryCell", for: indexPath)
+
+        cell.textLabel?.text = EntryController.shared.entries[indexPath.row].title
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            guard let index = tableView.indexPathForSelectedRow else {return}
+            let entry = EntryController.shared.entries[index.row]
+            EntryController.shared.delete(entry: entry) { (success) in
+                DispatchQueue.main.async {
+                    self.tableView.deleteRows(at: [indexPath], with: .fade)
+                }
+            }
+        }
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
